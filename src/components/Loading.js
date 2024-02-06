527b93a3a74e68bead269aba030e5dd9bd555e44
Manas <iamscientistmanas@gmail.com>
2024-02-06 11:28:02 +0530

    Add: loading

---
+++ src/components/Loading.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import Lottie from 'react-lottie';
+import loading_animation from './assets/animations/loader_animation.json';
+import './styles/loading.css';
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loading_animation,
+    rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+    },
+};
+
+const Loading = () => {
+    const [dots, setDots] = useState(1);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDots((prevDots) => (prevDots % 3) + 1);
+        }, 300);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="loading min-h-screen flex justify-center items-center flex-col bg-gradient-to-br from-[#cc2b5e] to-[#753a88]">
+            <div className='lottie relative'>
+                <Lottie
+                    options={defaultOptions}
+                    height={230}
+                    width={230}
+                />
+            </div>
+            <div className="loading-text select-none">
+                Loading{Array.from({ length: 3 }).map((_, index) => <span key={index} style={{ opacity: index < dots ? 1 : 0 }}>.</span>)}
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
